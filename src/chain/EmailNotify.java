3d95bef3ad7490f9552e99c711291c6de5df1d21
dilnaza <dilnazbaidakhanova@gmail.com>
2024-01-07 16:48:14 +0600

    chain

---
+++ src/chain/EmailNotify.java
@@ -0,0 +1,12 @@
+package chain;
+
+public class EmailNotify extends Notifier {
+    public EmailNotify(int priority) {
+        super(priority);
+    }
+
+    @Override
+    public void write(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
