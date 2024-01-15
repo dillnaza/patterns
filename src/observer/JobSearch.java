eaa9f30b933cffb239217fdc0e327d0f2aae8a8d
dilnaza <dilnazbaidakhanova@gmail.com>
2024-01-15 11:47:54 +0600

    observer

---
+++ src/observer/JobSearch.java
@@ -0,0 +1,17 @@
+package observer;
+
+public class JobSearch {
+    public static void main(String[] args) {
+        JavaDeveloperJobSite jobSite = new JavaDeveloperJobSite();
+        jobSite.addVacancy("First Java Position");
+        jobSite.addVacancy("Second Java Position");
+
+        Observer firstSubscriber = new Subscriber("Dilnaza Baidakhanova");
+        Observer secondSubscriber = new Subscriber("Dilnaza");
+
+        jobSite.addObserver(firstSubscriber);
+        jobSite.addObserver(secondSubscriber);
+
+        jobSite.addVacancy("Third Java Position");
+    }
+}
